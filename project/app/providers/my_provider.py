cf6faeda33c830087697471ff3019940b4e96daa
LuisGMM <69197163+LuisGMM@users.noreply.github.com>
2024-02-22 16:51:52 +0100

    Uncomment bug

--- project/app/providers/my_provider.py
+++ project/app/providers/my_provider.py
@@ -1,9 +1,9 @@
 class MyProvider:
     async def get(self) -> list[str]:
-        # from app.models import MyModel
-        #
-        # print("afirst inside gather")
-        # await MyModel.objects.afirst()
-        # print("afirst inside gather done")
+        from app.models import MyModel
+
+        print("afirst inside gather")
+        await MyModel.objects.afirst()
+        print("afirst inside gather done")
 
         return ["all ok"]
